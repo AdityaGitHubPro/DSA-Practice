0818449b5e56e3d7b882678e8c982fb5376774fe
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-02-17 22:52:17 +0530

    Create Binary Search.java

---
+++ Binary Search.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static int binarySearch(int numbers[], int key) {
+        int start = 0;
+        int end = numbers.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (numbers[mid] == key) {
+                return mid;
+            }
+            if (numbers[mid] < key) {
+                start = mid + 1;
+            }
+            if (numbers[mid] > key) {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14 };
+        int key = 12;
+        System.out.println("Infdex for key is " + binarySearch(numbers, key));
+    }
+}
