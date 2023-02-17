b3da48c8d6e6dca82466b0f0d5cad60535235ddb
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-02-17 22:54:37 +0530

    Create Pairs in an Array.java

---
+++ Pairs in an Array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void printPairs(int numbers[]) {
+        int tp = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] + ")");
+                tp++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total no of pairs " + tp);
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8 };
+        printPairs(numbers);
+    }
+}
